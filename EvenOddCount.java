5449e2f6957582466ec22752a1b2bdbd1f8d2d79
mahiila <mahiila49@gmail.com>
2025-09-19 22:15:29 +0600

    Array problem 5

---
+++ EvenOddCount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOddCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int even = 0, odd = 0;
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] % 2 == 0) even++;
+            else odd++;
+        }
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
+}
