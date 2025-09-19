b3bc99b7874f28f9676ea34030fd58871ffd0f2a
mahiila <mahiila49@gmail.com>
2025-09-19 21:41:08 +0600

    Problem 4

---
+++ Problem4_MaxOf3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Problem4_MaxOf3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        System.out.println("Maximum number is: " + max);
+        sc.close();
+    }
+}
