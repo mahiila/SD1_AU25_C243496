0ee0af5c937e9e08823d7e2e24b5a1f6ff26b305
mahiila <mahiila49@gmail.com>
2025-09-19 22:27:59 +0600

    Array problem 9

---
+++ VowelConsonantCount.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class VowelConsonantCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+        int vowels = 0, consonants = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                if ("aeiou".indexOf(c) != -1) vowels++;
+                else consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
